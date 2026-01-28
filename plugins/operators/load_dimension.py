8348ee774489077cd151d46c438945db35d0b8c5
STenhulzen <sam@wearefluence.com>
2026-01-28 13:48:08 -0700

    Update load_dimension.py

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -1,1 +1,29 @@
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+class LoadDimensionOperator(BaseOperator):
+    ui_color = "#80BD9E"
+
+    @apply_defaults
+    def __init__(
+        self,
+        redshift_conn_id="redshift",
+        table="",
+        sql_insert="",
+        mode="append",
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_insert = sql_insert
+        self.mode = mode
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.mode == "delete-load":
+            redshift.run(f"DELETE FROM {self.table}")
+
+        redshift.run(self.sql_insert)
