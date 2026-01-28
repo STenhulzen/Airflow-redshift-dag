200d04711b14ded75aafd8b93770eb0543ec269d
STenhulzen <sam@wearefluence.com>
2026-01-28 13:49:26 -0700

    Update sql_queries.py

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,1 +1,10 @@
+class SqlQueries:
+    songplay_table_insert = "INSERT INTO songplays SELECT ...;"
+    user_table_insert = "INSERT INTO users SELECT ...;"
+    song_table_insert = "INSERT INTO songs SELECT ...;"
+    artist_table_insert = "INSERT INTO artists SELECT ...;"
+    time_table_insert = "INSERT INTO time SELECT ...;"
 
+    quality_checks = [
+        {"sql": "SELECT COUNT(*) FROM songplays WHERE playid IS NULL", "expected": 0}
+    ]
