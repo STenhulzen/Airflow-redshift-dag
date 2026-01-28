d1f1f0cd24ce6f9598c395df1db43d15d6cbae71
STenhulzen <sam@wearefluence.com>
2026-01-28 13:48:30 -0700

    Update load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -1,1 +1,23 @@
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+class LoadFactOperator(BaseOperator):
+    ui_color = "#F98866"
+
+    @apply_defaults
+    def __init__(
+        self,
+        redshift_conn_id="redshift",
+        table="",
+        sql_insert="",
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_insert = sql_insert
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        redshift.run(self.sql_insert)
