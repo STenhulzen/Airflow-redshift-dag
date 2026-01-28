c06504dead3fa06a30eac07afe4f7b0e01661448
STenhulzen <sam@wearefluence.com>
2026-01-28 13:48:55 -0700

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -1,1 +1,25 @@
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
+class DataQualityOperator(BaseOperator):
+    ui_color = "#89DA59"
+
+    @apply_defaults
+    def __init__(
+        self,
+        redshift_conn_id="redshift",
+        tests=None,
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tests = tests or []
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        for test in self.tests:
+            records = redshift.get_records(test["sql"])
+            if records[0][0] != test["expected"]:
+                raise ValueError("Data quality check failed")
