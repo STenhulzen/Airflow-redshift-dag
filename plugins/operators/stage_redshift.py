2494353a180028015c1c7e467ae64ce850fc681c
STenhulzen <sam@wearefluence.com>
2026-01-28 13:46:27 -0700

    Update stage_redshift.py

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,1 +1,54 @@
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
 
+class RedshiftStageOperator(BaseOperator):
+    ui_color = "#358140"
+
+    @apply_defaults
+    def __init__(
+        self,
+        redshift_conn_id="redshift",
+        aws_credentials_id="aws_credentials",
+        table="",
+        s3_bucket="",
+        s3_key="",
+        json_path="auto",
+        region="us-west-2",
+        truncate_before_load=True,
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_path = json_path
+        self.region = region
+        self.truncate_before_load = truncate_before_load
+
+    def execute(self, context):
+        self.log.info("Staging data into %s", self.table)
+
+        aws_hook = AwsBaseHook(self.aws_credentials_id, client_type="sts")
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        rendered_key = self.s3_key.format(**context)
+        s3_path = f"s3://{self.s3_bucket}/{rendered_key}"
+
+        if self.truncate_before_load:
+            redshift.run(f"TRUNCATE TABLE {self.table}")
+
+        copy_sql = f"""
+            COPY {self.table}
+            FROM '{s3_path}'
+            ACCESS_KEY_ID '{credentials.access_key}'
+            SECRET_ACCESS_KEY '{credentials.secret_key}'
+            REGION '{self.region}'
+            FORMAT AS JSON '{self.json_path}';
+        """
+
+        redshift.run(copy_sql)
